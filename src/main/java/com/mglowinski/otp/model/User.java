2a12ef59342bfd9da10fd0abf60dc2edc1f04d89
Maciej Głowiński <maciej.glowinski@comarch.pl>
2019-03-26 11:06:49 +0100

    add models

---
+++ src/main/java/com/mglowinski/otp/model/User.java
@@ -0,0 +1,28 @@
+package com.mglowinski.otp.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private Set<Role> roles;
+}
